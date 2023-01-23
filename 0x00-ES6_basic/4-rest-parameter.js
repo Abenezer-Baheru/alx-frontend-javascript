8d7c4ef0f35120358c4e56d3e86986a58a914e86
Abenezer Baheru <101478749+Abenezer-Baheru@users.noreply.github.com>
2023-01-23 21:26:33 +0300

    Delete 0x00-ES6_basic directory
    yes

--- 0x00-ES6_basic/4-rest-parameter.js
+++
@@ -1,9 +0,0 @@
-export default function returnHowManyArguments(...args) {
-  let count = 0;
-
-  // eslint-disable-next-line no-unused-vars
-  for (const _ of args) {
-    count += 1;
-  }
-  return count;
-}
